901129b49648612535d4f8661721e38c74d14626
Leonardo Garcia <leogsantos2017@gmail.com>
2023-08-25 09:34:16 -0300

    EcoAgro excel file is just a .html

--- scraper.py
+++ scraper.py
@@ -2,42 +2,16 @@ import pandas as pd
 from bs4 import BeautifulSoup
 from requests import get
 
-def getTable(cetip, id):
-    url = "https://www.ecoagro.agr.br/historico-pu/" + id + '/' + cetip
-    response = get(url)
-    html_soup = BeautifulSoup(response.text, 'html.parser')
-    table = html_soup.find('table', class_="current__table")
 
-    table_head = table.find('thead')
-    headers = table_head.find_all('th')
-
-    out_headers = []
-
-    for header in headers:
-        out_headers.append(header.text)
-
-    out = []
-
-    rows = table.find_all("tr")
-
-    for row in rows:
-        fields = row.find_all('td')
-        out_row = []
-        for field in fields:
-            out_row.append(field.text)
-        out.append(out_row)
-
-    df = pd.DataFrame(out, columns=out_headers)
-    return df
-
-def downloadXlsBackup(cetip, id):
-    response = get("https://www.ecoagro.agr.br/excel-historico-pu/129/CRA0190066O")
+def downloadXls(cetip, id):
+    response = get("https://www.ecoagro.agr.br/excel-historico-pu/" + id + '/' + cetip)
     with open(cetip + '.xls', 'wb') as out:
         out.write(response.content)
+        out.close()
+    return pd.read_html(response.content, header=0)
 
 def cleanTable(df):
     df["Data"] = df['Data'].str.replace('.','/', regex=False)
-    df["Data"] = df['Data'].str[3:6] + df['Data'].str[0:3] + df['Data'].str[6:]
     df = df.loc[::-1].reset_index(drop=True)
     return df
 
@@ -47,21 +21,22 @@ def getId(cetip):
 
     soup = BeautifulSoup(response.text, 'html.parser')
 
-    elements = soup.find_all(attrs={"data-id":True})
-    if len(elements) != 1:
-        print("deu ruim")
-        return
-    else:
-        return elements[0]['data-id']
+    rows = soup.find_all("tr")
+
+    for row in rows:
+        fields = row.find_all('td')
+        for field in fields:
+            if field.text.find(cetip) != -1:
+                return row['data-id']
 
 cetip = input("CETIP do fundo: ")
 
 id = getId(cetip)
 
-df = getTable(cetip, id)
+df = downloadXls(cetip, id)
+
+print(df)
 
 df = cleanTable(df)
 
 df.to_csv(cetip + ".csv", index=False)
-
-downloadXlsBackup(cetip, id)
